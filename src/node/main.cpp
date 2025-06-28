6dec3a12b8b59751eb04b5b71d526f2ed57df901
Emiliano <emiarnedo@gmail.com>
2025-06-28 20:07:06 -0300

    Prueba de libreria y error de compilación esperado

---
+++ src/node/main.cpp
@@ -0,0 +1,39 @@
+/************************************************************************************************
+Copyright (c) 2025, Emiliano Arnedo
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction...
+SPDX-License-Identifier: MIT
+************************************************************************************************/
+
+/** \brief Nodo ESP32 basado en ESP-NOW y la biblioteca ZHNetwork
+ **
+ ** Estructura general del firmware principal. Preparado para integrar funciones específicas como
+ ** lectura de sensores, transmisión de datos y multitarea con FreeRTOS.
+ **/
+
+
+// === Constantes y configuraciones iniciales ================================================= //
+
+
+// === Prototipos de funciones ================================================================= //
+
+
+// === Variables globales ====================================================================== //
+
+
+// === Setup =================================================================================== //
+
+void setup() {
+
+}
+
+// === Loop ==================================================================================== //
+
+void loop() {
+
+}
+
+// === Implementación de funciones ============================================================= //
+
+
+// === Funciones auxiliares internas =========================================================== //
